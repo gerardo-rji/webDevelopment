0d16a2b815b62548d410b569eeabaf0020e61d54
Gerardo_RJI <gera.rji96@gmail.com>
2025-08-13 16:30:30 -0600

    Add token authentication

--- FSO/Part3/SimpleWebServer/controllers/notes.js
+++ FSO/Part3/SimpleWebServer/controllers/notes.js
@@ -1,6 +1,15 @@
+const jwt = require('jsonwebtoken')
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 notesRouter.get('/', async (request, response) => {
   /*Note.find({}).then(notes => {
@@ -32,17 +41,20 @@ notesRouter.get('/:id', async (request, response) => {
 
 notesRouter.post('/', async (request, response) => {
   const body = request.body
-
-  const user = await User.findById(body.userId)
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   if (!user) {
-    return response.status(400).json({ error: 'userId missing or not valid' })
+    return response.status(400).json({ error: 'UserId missing or not valid' })
   }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
-    user: user.id
+    user: user._id
   })
 
   const savedNote = await note.save()
